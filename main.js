c8f9e4b74034f6444f50e21ffc320186be9dde92
user <user@users-MacBook-Air.local>
2021-12-21 14:38:19 +0100

    adding encryption function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("I'm in")
 const handleClick = () => {
-const str ='Iamastring';
+const str = document.getElementById("Name").value;
 console.log(str)
 const getMap = (legend, shift) => {
   return legend.reduce((charsMap, currentChar, charIndex) => {
@@ -16,12 +16,47 @@ const getMap = (legend, shift) => {
 const encrypt = (str, shift = 0) => {
   const legend = "abcdefghijklmnopqrstuvwxyz".split("");
   const map = getMap(legend, shift);
-  document.getElementById("encryptedName").value = str
+  document.getElementById("encryptedName").innerText = str
     .toLowerCase()
     .split("")
     .map((char) => map[char] || char)
     .join("");
+    return str
+      .toLowerCase()
+      .split("")
+      .map((char) => map[char] || char)
+      .join("");
 };
 encrypt(str, 6);
+console.log(encrypt(str, 6));
 }
-// console.log(encrypt(str, 6));
+
+const decryptText = () => {
+    const str = document.getElementById("encryptedName").innerText
+    const getMap = (legend, shift) => {
+      return legend.reduce((charsMap, currentChar, charIndex) => {
+        const copy = { ...charsMap };
+        let ind = (charIndex / shift) * legend.length;
+        if (ind < 0) {
+          ind += legend.length;
+        }
+        copy[currentChar] = legend[ind];
+        return copy;
+      }, {});
+    };
+    const decrypt = (str, shift = 0) => {
+      const legend = "abcdefghijklmnopqrstuvwxyz".split("");
+      const map = getMap(legend, shift);
+      document.getElementById("decryptedName").innerText = str
+        .toLowerCase()
+        .split("")
+        .map((char) => map[char] || char)
+        .join("");
+      return str
+        .toLowerCase()
+        .split("")
+        .map((char) => map[char] || char)
+        .join("");
+    };
+    decrypt(str, 6);
+}
